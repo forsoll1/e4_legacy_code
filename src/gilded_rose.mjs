b46f815763b0e30816aa0e8d83394e6d92a9052d
Olli Forssell <olli20112@student.hamk.fi>
2024-02-22 19:47:35 +0200

    Random item tests

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -57,7 +57,6 @@ export class Shop {
         }
       }
     }
-
     return this.items;
   }
 }
--- test/gilded_rose.test.mjs
+++ test/gilded_rose.test.mjs
@@ -3,9 +3,36 @@ import { expect } from "chai";
 import { Item, Shop } from "../src/gilded_rose.mjs";
 
 describe("Gilded Rose", () => {
-  test("foo", () => {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+/*
+RULES: 
+- Quality always between 0 and 50
+- Random items go down in quality with each update
+- Aged Brie goes up in quality with each update
+- Backstage pass goes up in quality by one if sellIn >= 11
+- Backstage pass goes up in quality by two if 5 <= sellIn < 11
+- Backstage pass goes up in quality by three if sellIn < 5
+- Backstage pass quality == 0 if sellIn < 0
+- Sulfuras sellIn value does not decrease
+- Sulfuras quality does not decrease
+- All other items sellIn value decreases by -1 with update
+*/
+
+  test("Random item quality decreases", () => {
+    const gildedRose = new Shop([new Item("TestItem", 2, 50)]);
     const items = gildedRose.updateQuality();
-    expect(items[0].name).to.equal("foo");
+    expect(items[0].quality).to.equal(49);
   });
+
+  test("Random item quality decreases but not below 0", () => {
+    const gildedRose = new Shop([new Item("TestItem", 2, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].quality).to.equal(0);
+  });
+
+  test("Random item sellIn decreases", () => {
+    const gildedRose = new Shop([new Item("TestItem", 10, 48)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].sellIn).to.equal(9);
+  });
+
 });
