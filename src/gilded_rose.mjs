9eb7499f8b0c322afeb18175e0b417db4534234c
Olli Forssell <olli20112@student.hamk.fi>
2024-02-24 22:08:19 +0200

    Refactoring done

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -18,39 +18,32 @@ export class Shop {
         this.handleBackstagePass(item)
         continue
       }
-      if (item.name === "Aged Brie" && item.quality < 50) {
+      if (item.name === "Aged Brie") {
         this.handleAgedBrie(item)
         continue
       }
-
-      if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
-        if (item.quality > 0) {
-          this.decreaseQuality(item);
-        }
-      } 
-
-
-      this.decreaseSellIn(item);
-      
-      if (item.sellIn < 0) {
-        if (item.name != "Aged Brie") {
-          if (item.quality > 0) {
-            this.decreaseQuality(item);
-          }
-        }
-      }
+      this.handleRandomItem(item)
     }
     return this.items;
   }
+
   increaseQuality(item){
-    item.quality += 1;
+    if(item.quality < 50) { item.quality += 1 };
   }
   decreaseQuality(item){
-    item.quality -= 1;
+    if(item.quality > 0){ item.quality -= 1 };
   }
   decreaseSellIn(item){
     item.sellIn -= 1
   }
+  handleRandomItem(item){
+    this.decreaseQuality(item);
+    this.decreaseSellIn(item)
+    if (item.sellIn < 0) {
+      this.decreaseQuality(item);
+    }
+  }
+
   handleAgedBrie(item){
     this.increaseQuality(item)
     this.decreaseSellIn(item)
@@ -59,18 +52,15 @@ export class Shop {
     }
   }
 
-
   handleBackstagePass(item){
     this.backstagePassQuality(item)
     this.decreaseSellIn(item);
-    if(item.sellIn <= 0){item.quality = 0; return}
+    if(item.sellIn <= 0){item.quality = 0}
   }
+
   backstagePassQuality(item){
-    let currentQuality = item.quality
-    if(item.sellIn > 10){ currentQuality += 1 }
-    else if(item.sellIn > 5) { currentQuality += 2 }
-    else currentQuality += 3
-    if(currentQuality > 50) {currentQuality = 50}
-    item.quality = currentQuality
+    this.increaseQuality(item)
+    if(item.sellIn < 11) { this.increaseQuality(item)}
+    if(item.sellIn < 6) { this.increaseQuality(item) }
   }
 }
