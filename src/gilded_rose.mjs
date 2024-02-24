e6187e57e6806c877362e852de88fb3185c85000
Olli Forssell <olli20112@student.hamk.fi>
2024-02-24 21:15:30 +0200

    Sulfuras qualifier comes up first in stack, Sulfuras-conditions removed elsewhere. Backstage Pass special quality increases moved to separate method

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -10,51 +10,37 @@ export class Shop {
   constructor(items = []) {
     this.items = items;
   }
-
-
   
   updateQuality() {
     for (const item of this.items) {
+      if(item.name === "Sulfuras, Hand of Ragnaros") {continue}
       if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
         if (item.quality > 0) {
-          if (item.name != "Sulfuras, Hand of Ragnaros") {
-            item.quality = item.quality - 1;
-          }
+          this.decreaseQuality(item);
         }
       } else {
         if (item.quality < 50) {
-          item.quality = item.quality + 1;
+          this.increaseQuality(item);
           if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
+            this.handleBackstagePass(item)
           }
         }
       }
-      if (item.name != "Sulfuras, Hand of Ragnaros") {
-        item.sellIn = item.sellIn - 1;
-      }
+      this.decreaseSellIn(item);
       if (item.sellIn < 0) {
         if (item.name != "Aged Brie") {
           if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
             if (item.quality > 0) {
               if (item.name != "Sulfuras, Hand of Ragnaros") {
-                item.quality = item.quality - 1;
+                this.decreaseQuality(item);
               }
             }
           } else {
-            item.quality = item.quality - item.quality;
+            item.quality = 0;
           }
         } else {
           if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            this.increaseQuality(item);
           }
         }
       }
@@ -69,5 +55,13 @@ export class Shop {
   }
   decreaseSellIn(item){
     item.sellIn -= 1
+  }
+  handleBackstagePass(item){
+    if (item.sellIn < 11 && item.quality < 50) {
+      this.increaseQuality(item);
+    }
+    if (item.sellIn < 6 && item.quality < 50) {
+      this.increaseQuality(item);
+    }
   }
 }
